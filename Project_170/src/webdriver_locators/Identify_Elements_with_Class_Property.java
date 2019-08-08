7c1c47f92474c34375f27031d92a4d0b389b117d
Administrator <Administrator@192.168.2.6>
2019-08-08 11:03:39 +0530

    css and class name

---
+++ Project_170/src/webdriver_locators/Identify_Elements_with_Class_Property.java
@@ -0,0 +1,41 @@
+package webdriver_locators;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Identify_Elements_with_Class_Property {
+
+	public static void main(String[] args)
+	{
+		
+		//Locate chrome browser in currnet system
+		System.setProperty("webdriver.chrome.driver", "Drivers\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();  //Launch browser
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https://www.outlook.com");    //Load webpage
+		driver.manage().window().maximize();  //maximize browser window
+		
+		
+		
+		//Identify location with class property
+		driver.findElement(By.className("linkButtonSigninHeader")).click();
+		
+
+		/*
+		 * Identify location with class property when define along with spaces:-->
+		 * 
+		 *   <input class="ltr_override liveDomainInput col-xs-14">
+		 *   
+		 *   Note:--> don't define class property along with spaces
+		 */
+		driver.findElement(By.className("liveDomainInput")).clear();
+		driver.findElement(By.className("liveDomainInput")).sendKeys("info.dsnr123");
+		
+		
+		driver.findElement(By.id("iSignupAction")).click();
+	}
+
+}
